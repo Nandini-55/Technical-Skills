66dc975f8e535cc3d4708b1987f770109d0e19fc
Nandini-55 <njkstars1212@gmail.com>
2026-03-28 11:51:32 +0530

    View here

---
+++ src/RedundantBraces.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RedundantBraces {
+    // given a string a denoting an expression it contains the following operators
+    // -'+','-','*','/'
+    // check whether a has redundant braces or not
+    // note = a will be always a valid expression and will not contain any white
+    // spaces
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter expression : ");
+        String a = src.next();
+        Stack<Character> st = new Stack<>();
+        int res = 0;
+        char last;
+        for (int i = 0; i < a.length(); i++) {
+            char ch = a.charAt(i);
+            if (ch == ')') {
+                int count = 0;
+
+                while (!st.isEmpty() && st.peek()!='(') {
+                    st.pop();
+                    count++;
+                }
+                st.pop();
+                if(count==0){
+                    res=1;
+                    break;
+                }
+            } else if(ch=='/' || ch=='*' || ch=='-' || ch=='+' || ch=='(') {
+                st.push(ch);
+            }
+        }
+
+        if(res==0){
+            System.out.println("No redundant braces");
+        }else{
+            System.out.println("Has Redundant braces");
+        }
+    }
+}
---
+++ src/RemoveChar.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RemoveChar {
+    // You have a string denoted as 'a' to transform the string, you have to perform
+    // the following operations:
+    // 1. identify the first occurrence of consecutive identical pairs of characters
+    // with in the string
+    // 2. remove this pair of identical characters from the string
+    // 3. repeat step 1 and 2 until their are no more consecutive identical pairs of
+    // characters
+    // 4. the final result will be the transformed string.
+
+    // solution :
+    // create an empty stack
+    // traverse the string from left to right
+    // for each step if stack is not empty and top of the stack is equals to the
+    // current character , then remove the top element ,else push the current
+    // character into the stack.
+    // repeat until all character are traversed and convert stack to string
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter string: ");
+        String A = src.nextLine();
+        // String A = "abccbc";
+        StringBuilder res = new StringBuilder("");
+        Stack<Character> ch = new Stack<>();
+        ch.push(A.charAt(0));
+        for (int i = 1; i < A.length(); i++) {
+
+            if (!ch.isEmpty() && ch.peek() == A.charAt(i)) {
+                ch.pop();
+            } else {
+                ch.push(A.charAt(i));
+            }
+        }
+        while (!ch.isEmpty()) {
+            res.append(ch.pop());
+        }
+
+        res.reverse();
+        System.out.println(res);
+
+    }
+}
