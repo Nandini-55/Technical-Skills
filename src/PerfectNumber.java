432ae18ff6cf0242771a13a5adb4e5d2c91ba09b
Nandini-55 <njkstars1212@gmail.com>
2026-04-09 19:51:01 +0530

    View here

---
+++ src/PerfectNumber.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+public class PerfectNumber {
+    // Given and integer A , you have to find the Ath Perfect Number.
+    // A perfect number has the following properties:
+    // 1. It comprises only one and two.
+    // 2. The number of digits in a perfect number is even.
+    // 3. It is a palindrome number.
+    // For example , 11,22,112211 are perfect numbers, where 123, 121, 728, 1 are
+    // not.
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter the value of A: ");
+        int A = src.nextInt();
+        // Create a queue
+        Queue<String> q = new ArrayDeque<>();
+        // Insert "1" and "2";
+        q.add("1");
+        q.add("2");
+
+        // Repeat A times:
+        for (int i = 0; i < A; i++) {
+            StringBuilder temp = new StringBuilder(q.poll());
+            String t = temp.toString();
+            String palin = t;
+            palin +=temp.reverse();
+            System.out.print(palin+" ");
+            q.add(t+"1");
+            q.add(t+"2");
+        }
+
+    }
+}
